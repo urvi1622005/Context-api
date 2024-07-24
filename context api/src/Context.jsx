0487d7416475a5bb536b8356c133160c9b6f4a70
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-07-24 08:14:42 +0530

    .

--- context api/src/Context.jsx
+++ context api/src/Context.jsx
@@ -2,7 +2,8 @@ import React,{createContext,useState} from 'react';
 const Context = createContext();
 const Provider =({ children }) => {
     const[name,setState]=useState("urvi");
-    const[age,setAge]=useState("19")
+    const[age,setAge]=useState("19");
+    
 
     return(
         <Context.Provider value={{name,age,setAge,setState}}>
