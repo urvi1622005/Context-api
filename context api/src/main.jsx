65508a2b5c0284c5b82051a51cbe08b587bc5ddf
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-05 23:15:43 +0530

    ..

--- context api/src/main.jsx
+++ context api/src/main.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from './Context.jsx'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { Provider } from './Context.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Provider>
+//     <App />
+//     </Provider>
+//   </React.StrictMode>,
+// )
+import React from 'react'
+import {Provider} from './Context'
+import App from './App.jsx'
+import ReactDom from 'react-dom/client'
+
+ReactDom.createRoot(document.getElemnentById('root')).render(
   <React.StrictMode>
     <Provider>
-    <App />
+      <App/>
     </Provider>
-  </React.StrictMode>,
+  
+  </React.StrictMode>
 )
