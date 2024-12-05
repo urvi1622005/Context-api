aea38d982ea572c220cd1aa4ac09f33fabdc4cd8
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-05 23:15:25 +0530

    ..

--- context api/src/App.jsx
+++ context api/src/App.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import{Provider} from './Context';
-import './App.css';
-import Component from './Component';
+// import React from 'react';
+// import{Provider} from './Context';
+// import './App.css';
+// import Component from './Component';
 
-function App ()  {
+// function App ()  {
   
 
-  return (
+//   return (
+//     <Provider>
+//      <Component/>
+//     </Provider>
+//   );
+// }
+
+// export default App;
+import React from 'react'; 
+import {Provider} from './Context ';
+import Component from './Component';
+
+function App () {
+  return(
     <Provider>
-     <Component/>
+      <Component/>
     </Provider>
   );
 }
-
-export default App;
