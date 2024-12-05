024eebfa95a28192542ec84529824f3138df7a72
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-05 23:15:33 +0530

    ..

--- context api/src/Component.jsx
+++ context api/src/Component.jsx
@@ -1,20 +1,33 @@
+// import React,{useContext} from 'react';
+// import { Context } from './Context.jsx';
+
+// const Component = () => {
+
+//     const {name,setName} = useContext(Context);
+//     const {age,setAge} = useContext(Context);
+
+//   return (
+//     <div>
+//         <p>{name}</p>
+//         <p>{age} </p>
+//         <button onClick={() => setName("new value")}>Change Value</button>
+//         <button onClick={() => setAge("new age")}>Change Age</button>
+//     </div>
+//   );
+
+// };
+
+// export default Component;
 import React,{useContext} from 'react';
 import { Context } from './Context.jsx';
-
-const Component = () => {
-
-    const {name,setName} = useContext(Context);
-    const {age,setAge} = useContext(Context);
-
-  return (
+const Component =() => {
+  const {name,age,setAge,setName}=useContext(Context);
+  return(
     <div>
-        <p>{name}</p>
-        <p>{age} </p>
-        <button onClick={() => setName("new value")}>Change Value</button>
-        <button onClick={() => setAge("new age")}>Change Age</button>
+      <p>{name}</p>
+      <p>{age}</p>
+      <button onClick={() => setName("new value")}>Change Value</button>
+      <button onClick={() => setAge("new age")}>Change Age</button>
     </div>
-  );
-
-};
-
-export default Component;
+  )
+}
