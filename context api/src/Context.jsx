b6cfde3a49ab64b92aeb150f2377617ea5280e46
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-12-05 23:15:38 +0530

    ..

--- context api/src/Context.jsx
+++ context api/src/Context.jsx
@@ -1,15 +1,15 @@
-import React,{createContext,useState} from 'react';
-const Context = createContext();
-const Provider =({ children }) => {
-    const[name,setState]=useState("urvi");
-    const[age,setAge]=useState("19");
+// import React,{createContext,useState} from 'react';
+// const Context = createContext();
+// const Provider =({ children }) => {
+//     const[name,setState]=useState("urvi");
+//     const[age,setAge]=useState("19");
     
 
-    return(
-        <Context.Provider value={{name,age,setAge,setState}}>
-            {children}
-        </Context.Provider>
-    );
-};
+//     return(
+//         <Context.Provider value={{name,age,setAge,setState}}>
+//             {children}
+//         </Context.Provider>
+//     );
+// };
 
-export {Provider,Context};
+// export {Provider,Context};
